2e7e698a212246f44f95737dc8ee57d319cdc9f6
rok21 <rokas.grikietis@argusmedia.com>
2016-05-07 14:24:03 +0100

    working compress

--- compressSbt2/src/main/scala/RLECompressor.scala
+++ compressSbt2/src/main/scala/RLECompressor.scala
@@ -11,16 +11,34 @@ trait Compressor {
 }
 
 object RLECompressor extends Compressor{
-  override def compress[A]: (Seq[A]) => Seq[Compressed[A]] = {
-    def compressInner(paramSeq : Seq[A]) : Seq[Compressed[A]] = {
-      var compressedSeq = List[Compressed[A]]()
-      for(elem <- paramSeq){
-        compressedSeq = compressedSeq :+ new Single[A](elem)
+    override def compress[A]: (Seq[A]) => Seq[Compressed[A]] = {
+      def compressMain(paramSeq : Seq[A]) : Seq[Compressed[A]] = {
+        var compressedSeq = List[Compressed[A]]()
+        var currentRepeat: Repeat[A] = null
+
+        for (i <- paramSeq.indices) {
+          var latestCompressed: Compressed[A] = null
+          if (currentRepeat == null) { currentRepeat = new Repeat[A](0, paramSeq(i)) }
+          currentRepeat = new Repeat[A](currentRepeat.count+1, currentRepeat.element)
+
+          if (i== (paramSeq.length-1) || !paramSeq(i+1).equals(currentRepeat.element)) {
+            //last or next one is different
+            if (currentRepeat.count > 1) {
+              latestCompressed = currentRepeat
+            } else {
+              latestCompressed = new Single[A](currentRepeat.element)
+            }
+            currentRepeat = null
+          }
+
+          if(latestCompressed != null){
+            compressedSeq = compressedSeq :+ latestCompressed
+          }
+        }
+        compressedSeq
       }
-      compressedSeq
+      compressMain
     }
-    compressInner
-  }
 
   override def decompress[A]: (Seq[Compressed[A]]) => Seq[A] = ???
 }
--- compressSbt2/src/test/scala/CompressSuite.scala
+++ compressSbt2/src/test/scala/CompressSuite.scala
@@ -10,6 +10,15 @@ import org.scalatest.junit.JUnitRunner
 class CompressSuite extends FunSuite{
   import RLECompressor.compress
 
+  test("compress: empty sequence") {
+
+    val param: Seq[String] = List()
+
+    val result = compress(param)
+
+    assert(result.length == 0)
+  }
+
   test("compress: Single objects only: ints"){
 
     val param : Seq[Int] = List(1, 2, 3)
@@ -18,23 +27,68 @@ class CompressSuite extends FunSuite{
 
     assert(result.length == 3)
     for(i <- param.indices){
-      assert(result(i).isInstanceOf[Compressed[Int]])
       assert(result(i).asInstanceOf[Single[Int]].element.equals(param(i)))
     }
   }
 
-  test("compress: Single objects only: Strings"){
+  test("compress: Single objects only: Strings") {
 
-    val param : Seq[String] = List("AB", "A", "AA")
+    val param: Seq[String] = List("AB", "A", "AA")
 
     val result = compress(param)
 
     assert(result.length == 3)
-    for(i <- param.indices){
-      assert(result(i).isInstanceOf[Compressed[String]])
+    for (i <- param.indices) {
       assert(result(i).asInstanceOf[Single[String]].element.equals(param(i)))
     }
   }
 
+  test("compress: Single objects only: one element") {
+
+    val param: Seq[String] = List("AB")
+
+    val result = compress(param)
+
+    assert(result.length == 1)
+    assert(result(0).asInstanceOf[Single[String]].element.equals(param(0)))
+  }
+
+  test("compress: Repeat: example from wiki"){
+    val param: Seq[Char] = List('W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'B', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'B', 'B', 'B', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'B', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W', 'W')
+
+    val result = compress(param)
+
+    //12W 1B 12W 3B 24W 1B 14W
+    assert(result.length == 7)
+
+    assert(result(0).asInstanceOf[Repeat[Char]].count==12)
+    assert(result(0).asInstanceOf[Repeat[Char]].element=='W')
+
+    assert(result(1).asInstanceOf[Single[Char]].element=='B')
+
+    assert(result(2).asInstanceOf[Repeat[Char]].count==12)
+    assert(result(2).asInstanceOf[Repeat[Char]].element=='W')
+
+    assert(result(3).asInstanceOf[Repeat[Char]].element=='B')
+    assert(result(3).asInstanceOf[Repeat[Char]].count==3)
+
+    assert(result(4).asInstanceOf[Repeat[Char]].element=='W')
+    assert(result(4).asInstanceOf[Repeat[Char]].count==24)
+
+    assert(result(5).asInstanceOf[Single[Char]].element=='B')
+
+    assert(result(6).asInstanceOf[Repeat[Char]].element=='W')
+    assert(result(6).asInstanceOf[Repeat[Char]].count==14)
+  }
+
+  test("compress: Repeat: single repeat"){
+    val param : Seq[Int] = List(1, 1, 1)
+
+    val result = compress(param)
+
+    assert(result.length == 1)
+    assert(result(0).asInstanceOf[Repeat[Int]].count==3)
+    assert(result(0).asInstanceOf[Repeat[Int]].element==1)
+  }
 }
 
